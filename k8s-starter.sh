97117dc8b90161027452dce9267f61f82337a4e4
Shubham Girdhar <girdharshubham@hotmail.com>
2019-07-11 03:52:31 +0530

    AKS-1 | K8s starter script modified wrt to the kubeconfig file

--- k8s-starter.sh
+++ k8s-starter.sh
@@ -1,4 +1,4 @@
-kubectl create --kubeconfig=/etc/kubernetes/admin.conf -f kubernetes/akka-service/
+kubectl create --kubeconfig=/home/kubernetes/admin.conf -f kubernetes/akka-service/
 kubectl create --kubeconfig=/etc/kubernetes/admin.conf -f kubernetes/elasticsearch/
 kubectl create --kubeconfig=/etc/kubernetes/admin.conf -f kubernetes/kafka/
 kubectl create --kubeconfig=/etc/kubernetes/admin.conf -f kubernetes/zookeeper/
